e1097113bca445fc6f1adb542a9d9a9d84278bf0
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2020-11-14 15:43:04 +0200

    Змінює скрипт для завдання № 5

--- js/task-5.js
+++ js/task-5.js
@@ -1,6 +1,12 @@
 const inputRef = document.querySelector('#name-input');
 const titleSpanRef = document.querySelector('#name-output');
 
-inputRef.addEventListener('input', event => {
+inputRef.addEventListener('input', hadleTextEnter);
+
+function hadleTextEnter(event) {
   titleSpanRef.textContent = event.target.value;
-});
+
+  if (event.target.value === '') {
+    titleSpanRef.textContent = 'незнакомец';
+  }
+}
